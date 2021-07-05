8f51bf4619ac657d1751327ca5c741c519eb2270
Séan Poynter-Smith <spoyntersmith@users.noreply.github.com>
2021-07-05 12:16:03 +0100

    add disk config and package dependencies

--- src/PentangleLaravelBackupConfigClass.php
+++ src/PentangleLaravelBackupConfigClass.php
@@ -2,6 +2,26 @@
 
 namespace Pentangle\PentangleLaravelBackupConfig;
 
-class PentangleLaravelBackupConfigClass
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+class PentangleLaravelBackupConfigClass extends PackageServiceProvider
 {
+    public function configurePackage(Package $package): void
+    {
+        $package->name('pentangle-laravel-backup-config')
+            ->hasConfigFile(['backup']);
+
+        app()->config["filesystems.disks.pentangle-s3"] = [
+            'driver'   => 's3',
+            'key'      => config('backup.pentangle-s3.key'),
+            'secret'   => config('backup.pentangle-s3.secret'),
+            'region'   => config('backup.pentangle-s3.region'),
+            'bucket'   => config('backup.pentangle-s3.bucket'),
+            'url'      => config('backup.pentangle-s3.url'),
+            'endpoint' => config('backup.pentangle-s3.endpoint'),
+            'root'     => str_replace(['http://', 'https://'], '', config('app.url')),
+        ];
+
+    }
 }
